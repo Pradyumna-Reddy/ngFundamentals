b597c3efeeff08715f6dd8dbb02b27e48b242fc7
Pradyumna <pradyum2411@gmail.com>
2021-07-05 19:12:40 +0530

    PreventRoutes-RouteGuardActivator

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from "@angular/router";
+import { Error404Component } from "./errors/404.component";
+import { CreateEventComponent } from "./events/create-event.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 import { EventsListComponent } from "./events/events-list.component";
 
 
 export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: 'events/new', component: CreateEventComponent },
+  { path: 'events/:id', component: EventDetailsComponent,
+    canActivate: [EventRouteActivator] },
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: '404' , component: Error404Component}
 ]
