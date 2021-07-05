f33ac07c415b6e3c1fee93fc816c9962eac7dfba
Pradyumna <pradyum2411@gmail.com>
2021-07-05 17:53:11 +0530

    AddedRoutes

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,7 +3,6 @@ import { EventService } from "./shared/event.service";
 import { ToastrService } from "../common/toastr.service";
 
 @Component({
-  selector: 'events-list',
   template: `
   <div>
     <h1>Upcoming Angular Events</h1>
@@ -17,7 +16,7 @@ import { ToastrService } from "../common/toastr.service";
   `
 })
 export class EventsListComponent implements OnInit {
-  events: any[];
+  events: any[]
   constructor(private eventService: EventService,
     private toastr: ToastrService) {
     this.events = []
