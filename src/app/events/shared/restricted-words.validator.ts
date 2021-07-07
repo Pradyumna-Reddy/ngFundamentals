4a9dc2c343b1faa515b953030572cc2efb67a95f
Pradyumna <pradyum2411@gmail.com>
2021-07-07 18:12:44 +0530

    VeryCoolComponentCommXXCreatedNonReloadingPagechangeUsingIT

--- src/app/events/shared/restricted-words.validator.ts
+++ src/app/events/shared/restricted-words.validator.ts
@@ -1,9 +1,8 @@
-import { FormControl } from "@angular/forms"
+import { AbstractControl} from "@angular/forms"
 
 export function restrictWords(words: any[]){
-  return (control: FormControl): {[key: string]:any} | null => {
+  return (control: AbstractControl): {[key: string]:any} | null => {
     if (!words) return null
-
     var invalidWords = words
       .map(w => control.value.includes(w) ? w : null)
       .filter(w => w!=null)
