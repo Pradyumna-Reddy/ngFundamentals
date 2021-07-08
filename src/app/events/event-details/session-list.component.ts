a362bbe487f73cb88987e8928d654edf786acaf1
Pradyumna <pradyum2411@gmail.com>
2021-07-08 11:48:16 +0530

    FilterOnAction

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../shared";
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[] | undefined
+  @Input () filterBy: string = 'all'
+  visibleSessions: ISession[] | undefined
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy)
+    }
+  }
+
+  filterSessions(filter: string) {
+    if(filter === 'all') {
+      this.visibleSessions = this.sessions?.slice(0)
+    } else {
+      this.visibleSessions = this.sessions?.filter(session =>
+        session.level.toLocaleLowerCase() === filter)
+    }
+  }
 }
